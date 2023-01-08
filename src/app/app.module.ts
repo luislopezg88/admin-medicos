fb974aec1570541075e77d5fdeb0efd853283158
luislopezg88 <luislopezg88@gmail.com>
2023-01-08 11:53:48 -0400

    Modificacion en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,14 @@ import { NgModule } from '@angular/core';
 // Rutas
 import { APP_ROUTES } from './app.routes';
 
+//Modulos
+
+import { pagesModule } from './pages/pages.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { PegesComponent } from './pages/peges.component';
+
 
 
 @NgModule({
@@ -23,18 +19,13 @@ import { PegesComponent } from './pages/peges.component';
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    BreadcrumbsComponent,
-    SidebarComponent,
-    HeaderComponent,
-    ProgressComponent,
-    Graficas1Component,
-    PegesComponent
+   
+   
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    pagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,15 @@
 import {RouterModule, Routes } from "@angular/router";
-import { PegesComponent } from "./pages/peges.component";
+
 
 import { LoginComponent } from "./auth/login/login.component";
 import { RegisterComponent } from "./auth/register/register.component";
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
-import { Graficas1Component } from "./pages/graficas1/graficas1.component";
-import { NopagefoundComponent } from "./pages/nopagefound/nopagefound.component";
-import { ProgressComponent } from "./pages/progress/progress.component";
+import { NopagefoundComponent } from "./shared/nopagefound/nopagefound.component";
+
 
 
 
 const appRouter: Routes = [
-    {
-        path: '', 
-        component: PegesComponent,
-        children: [
-            {path: 'dashboard', component: DashboardComponent},
-            {path: 'progress', component: ProgressComponent},
-            {path: 'graficas1', component: Graficas1Component},
-            {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
-        ]
-    },
+    
 
     
     {path: 'login', component: LoginComponent},
